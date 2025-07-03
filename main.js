93b65c54c35faa2290f6fd993f26505bb789ad9a
kaylacpad <kaylacpadilla@gmail.com>
2025-07-02 22:22:45 -0700

    Added #BruceSpringsteen
    Added back into share link message

--- main.js
+++ main.js
@@ -4697,7 +4697,7 @@ function Nt(t) {
             "\n\n" +
             t +
             "\n\n" +
-            "#SpringsteenHeardle #Heardle @RockHeardles https://springsteenheardle.netlify.app/";
+            "#BruceSpringsteen #SpringsteenHeardle #Heardle @RockHeardles https://springsteenheardle.netlify.app/";
           if (
             !navigator.share ||
             !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
